a8796e2f322545a08a5715adf8ee4ceccca8221c
Jandepiel <jandepiel@hotmail.com>
2016-11-30 16:02:09 +0100

    Huiswerk3 bank dinges

--- Huiswerk3/assets/js/main.js
+++ Huiswerk3/assets/js/main.js
@@ -4,47 +4,32 @@ $(document).ready(function()
 	var box2 = document.getElementById('box2');
 	var box3 = document.getElementById('box3');
 	
-	var audio1 = new Audio('assets/audio/Sound1.mp3')
-	var audio2 = new Audio('assets/audio/Sound2.mp3')
-	var audio3 = new Audio('assets/audio/StartPacMan.mp3')
+	var bank1 = document.getElementById('bank1');
 	
-	box1.addEventListener('mouseenter', function()
+	
+	box1.addEventListener('click', function()
 	{
-		audio1.play();
+		$(bank1).attr('material','color: blue');
+	});
+
+	box2.addEventListener('click', function()
+	{
+		$(bank1).attr('material','color: red');
 	});
 	
-	box1.addEventListener('mouseleave', function()
+	box3.addEventListener('click', function()
 	{
-		audio1.pause();
-	});
-	
-	box2.addEventListener('mouseenter', function()
-	{
-		audio2.play();
-	});
-	
-	box2.addEventListener('mouseleave', function()
-	{
-		audio2.pause();
-	});
-	
-	box3.addEventListener('mouseenter', function()
-	{
-		audio3.play();
-	});
-	
-	box3.addEventListener('mouseleave', function()
-	{
-		audio3.pause();
+		$(bank1).attr('material','color: yellow');
 	});
 	
 	
 	
-	
+	// $(box).attr('width','100');
 	
 	
 	
 });
+
 
 
 
